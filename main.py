ccdb1564ed0b7a66a12b92f96ebdc9cbef8f5a8b
Lynx-theleaner <daffaauliad@gmail.com>
2025-09-12 20:56:45 +0800

    main app

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends
+import orm_models, database
+from sqlalchemy.orm import Session
+
+from app.models.v1 import (
+    AnggotaCreate, AnggotaResponse, AnggotaUpdate,
+    BukuCreate, BukuResponse, BukuUpdate,
+    KategoriCreate, KategoriResponse, KategoriUpdate,
+    PeminjamanCreate, PeminjamanResponse, PeminjamanUpdate,
+    PengembalianCreate, PengembalianResponse, PengembalianUpdate,
+    PetugasCreate, PetugasResponse, PetugasUpdate,
+    MessageResponse
+)
+
+from app.api.v1 import routers
+
+""""Inisialisasi aplikasi"""
+app = FastAPI(
+    title="E-Library API",
+    description="Sistem perpustakaan sederhana dengan FastAPI",
+    version="1.0.0"
+)
+
+"""Masukin Router"""
+for r in routers:
+    app.include_router(r)
+
+""""Awal halaman"""
+@app.get("/")
+def root():
+    return {"message": "Welcome to E-Library API 🚀"}
